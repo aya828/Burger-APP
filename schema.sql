3977a5988cfee055efcdc545646e7250c00d876e
aya <aya.yoshida8@gmail.com>
2020-02-12 19:35:41 -0500

    initial folder structure and npm installs

---
+++ schema.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE burger;
+USE burger;
---
+++ server.js
@@ -0,0 +1,34 @@
+// *** Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars as the default templating engine.
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+// require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
